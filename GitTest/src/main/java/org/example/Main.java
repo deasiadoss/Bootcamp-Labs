e11a8cb5b5b6b26c3da5a8e97b2476effa66bda6
deasiadoss <deasiacodes@gmail.com>
2024-04-09 15:11:25 -0500

    Tech Wizard

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is my Test new app");;
+        System.out.println("I am a master developer");
     }
 }
